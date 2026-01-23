ccb7f4eb96358b57eac63d55b6f16093938ab672
Matteo Hu <matteo.hu@edu.immaginazioneelavoro.it>
2026-01-23 15:36:23 +0100

    Add files via upload

--- FastAPI/lezione_07/mini_cinema/routers/admin.py
+++ FastAPI/lezione_07/mini_cinema/routers/admin.py
@@ -20,7 +20,9 @@ def root():
 @router.post("/random-seed")
 def random_seed():
     # 0 resettiamo cinema_hall
-    database.cinema_hall = init_cinema().copy
+    nuovi_dati = init_cinema()
+    cinema_hall.clear() # puliamo la lista ma manteniamo lo stesso indirizzo in memoria
+    cinema_hall.extend(nuovi_dati) # aggiungiamo i dati nuovi allo stesso indirizzo in memoria
     # per chiarezza, facciamo riferimento esplicito al file database per la nuova assegnazione
 
     # 1 troviamo il numero (casuale) di posti da prenotare
@@ -59,5 +61,45 @@ def media_di_gruppi(gruppi: list[list[dict]]) -> float:
         totale_gruppo = conta_prenotati(gruppo)
         lista_totali.append(totale_gruppo)
     return sum(lista_totali) / len(lista_totali)
+# todo fare la media con un solo for (abbastanza implicito- list comprehension)
+
+
+# 2.5 facciamo una funzione che estragga righe o colonne della sala dai dati
+def raggruppa(tipo="colonna"):
+    #controlliamo che il tipo sia tra quelli disponibili, senò diamo errore
+    assert tipo in {"colonna", "riga"}, "Il tipo di raggruppamento deve essere 'riga' o 'colonna'"
+    gruppi = {}
+
+    for record in cinema_hall:
+        chiave = record["number" if tipo == "colonna" else "row"] # otteniamo il nome della colonna/riga del posto corrente
+        # cerchiamo di prendere il gruppo con la stessa colonna dal dizionario
+        gruppo = gruppi.get(chiave, [])
+        gruppo.append(record)  # aggiungiamo il posto corrente all'elenco
+        gruppi[chiave] = gruppo # sovrascriviamo l'elenco nel dizionario
+
+    return gruppi
 
 # 3 applicare queste funzioni sia alle righe che alle colonne [da ottenere]
+@router.get("/stats")
+def calcola_stats():
+    righe = raggruppa("riga")
+    colonne = raggruppa("colonna")
+
+    media_righe = media_di_gruppi(value for value in righe.values()) # convertiamo dizionario in liste
+    media_colonne = media_di_gruppi(value for value in colonne.values())
+
+    conteggi_righe = {chiave: conta_prenotati(valore) for chiave, valore in righe.items()}
+    conteggi_colonne = {chiave: conta_prenotati(valore) for chiave, valore in colonne.items()}
+    #todo fare una funzione aggrega che data una tipologia di raggruppamento faccia i 3 passaggi sopra
+
+    return {
+        "righe": {
+            "media": media_righe,
+            "conteggi": conteggi_righe
+        },
+        "colonne": {
+            "media": media_colonne,
+            "conteggi": conteggi_colonne
+        },
+    }
+
